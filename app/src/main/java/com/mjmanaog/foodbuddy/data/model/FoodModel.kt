a8e60c8ac324eee3fa84c9fa08aa2befbeab71a7
Stepan Mednikov <x-steff@mail.ru>
2021-04-05 15:02:53 +0300

    перевёл

--- app/src/main/java/com/mjmanaog/foodbuddy/data/model/FoodModel.kt
+++ app/src/main/java/com/mjmanaog/foodbuddy/data/model/FoodModel.kt
@@ -12,38 +12,38 @@ data class FoodModel(
 
 val foodDummyData: ArrayList<FoodModel> = arrayListOf(
         FoodModel(
-                "Salmon Salad",
-                "One of the easiest equations for a wholesome meal is vegetables + salmon. Both are packed with nutrients like protein, healthy fats, and fiber that just make you feel good with every single bite, and this simple recipe is proof",
-                "80cal",
-                "4.5 rating",
+                "Салат с лососем",
+                "Лосось с овощами — идеальное сочетание для полноценного питания. Простой рецепт блюда, которое содержит в себе богатый набор питательных веществ: белки, жиры и клетчатку.",
+                "80 ккал",
+                "4.5",
                 R.drawable.img_salmon_salad
         ),
         FoodModel(
-                "BBQ Chicken Breast",
-                "BBQ chicken, whether cooked on the grill or baked in the oven, is one of those dinners nobody at the table will complain about.Unless it’s dry and overcooked. But that is NOT this recipe. It’s a basic anyone can easily master, so that they too can claim their BBQ chicken is the best on the block.",
-                "80cal",
-                "4.5 rating",
+                "Куриная грудка-барбикю",
+                "От курочки, приготовленной на гриле или запечённой в духовке все всегда в восторге, если только она не сухая или пережаренная.",
+                "80 ккал",
+                "4.5",
                 R.drawable.img_chicken
         ),
         FoodModel(
-                "Steamed Chicken and Rice",
-                "Steaming is an ideal method for cooking boneless chicken breasts and small whole birds such as Cornish hens. It retains the flavor, tenderness and moisture through the use of steam. It is a healthy method of cooking because no additional fat is used.",
-                "80cal",
-                "4.5 rating",
+                "Курица с рисом на пару",
+                "Приготовление на пару — здоровый метод приготовления пищи. Он сохраняет её аромат, нежность и полезные вещества. К тому же для приготовления блюд не используется масло.",
+                "80 ккал",
+                "4.5",
                 R.drawable.img_chicken_rice
         ),
         FoodModel(
-                "Caesar Salad",
-                "A green salad of romaine lettuce and croutons dressed with lemon juice (or lime juice), olive oil, egg, Worcestershire sauce, anchovies, garlic, Dijon mustard, Parmesan cheese, and black pepper.",
-                "80cal",
-                "4.5 rating",
+                "Салат Цезарь",
+                "Зелёный салат из листьев салата ромэн и гренок, заправленный лимонным соком (или соком лайма), оливковым маслом, яйцом, Вустерширским соусом, анчоусами, чесноком, дижонской горчицей, сыром Пармезан и чёрным перцем.",
+                "80 ккал",
+                "4.5",
                 R.drawable.img_salad
         ),
         FoodModel(
-                "Wholesome Meal",
-                "One of the easiest equations for a wholesome meal is vegetables + salmon. Both are packed with nutrients like protein, healthy fats, and fiber that just make you feel good with every single bite, and this simple recipe is proof",
-                "80cal",
-                "4.5 rating",
+                "Просто полезная еда",
+                "Лосось с овощами — идеальное сочетание для полноценного питания. Простой рецепт блюда, которое содержит в себе богатый набор питательных веществ: белки, жиры и клетчатку.",
+                "80 ккал",
+                "4.5",
                 R.drawable.img_healthy
         )
 )
